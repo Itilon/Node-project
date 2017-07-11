6235624412f3decf81631878b8fb0a37370e26ac
Itilon <ivanov.elect@gmail.com>
2017-07-12 01:24:23 +0300

    slider added - lagging at startup

---
+++ static/scripts/slider.js
@@ -0,0 +1,21 @@
+/* globals $ */
+
+let index = 0;
+
+setInterval( () => {
+    const slides = $('.slide');
+    let i;
+
+
+    for (i = 0; i < slides.length; i += 1) {
+        $(slides[i]).css('display', 'none');
+    }
+
+    index++;
+
+    if (index > slides.length) {
+        index = 1;
+    }
+
+    $(slides[index - 1]).css('display', 'block');
+}, 8000);
