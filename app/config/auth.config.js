6467a816b1bb734c3d8006b3c195c30e33452143
stefanmanev <stef_manev@abv.bg>
2017-07-17 19:34:22 +0300

    auth config

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -5,34 +5,38 @@ const { Strategy } = require('passport-local');
 
 
 const configAuth = (app, { users }) => {
-passport.use(new Strategy(
-  (username, password, done) => {
-   return users.findByUsername(username)
-    .then((user) => {
-        return done(null, user);
-    })
-    .catch((err) => {
-        return done(err);
+    passport.use(new Strategy(
+        (username, password, done) => {
+            return users.findByUsername(username)
+                .then((user) => {
+                    return done(null, user);
+                })
+                .catch((err) => {
+                    return done(err);
+                });
+        }
+    ));
+
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
     });
-  }
-));
 
-  app.use(cookieParser());
-  app.use(session({ secret: 'keyboard cat' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    return users.findById(id)
-        .then((user) => {
-            done(null, user);
-        })
-        .catch(done);
-});
+    passport.deserializeUser((id, done) => {
+        return users.findById(id)
+            .then((user) => {
+                done(null, user);
+            })
+            .catch(done);
+    });
 };
 
 module.exports = configAuth;
