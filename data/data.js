e58b3f035b6226f7ef705d7ae0d6349bc0c23169
Itilon <ivanov.elect@gmail.com>
2017-07-23 01:06:27 +0300

    Minor

--- data/data.js
+++ data/data.js
@@ -1,10 +1,12 @@
 const PostsData = require('./posts.data');
 const CategoriesData = require('./categories.data');
+const UsersData = require('./users.data');
 
 const init = (db) => {
     return Promise.resolve({
         items: new PostsData(db),
         categories: new CategoriesData(db),
+        users: new UsersData(db),
     });
 };
 
