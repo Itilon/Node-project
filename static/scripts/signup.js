70cd98c4b38ae77bbd67cebd645ad5db7fb58dcf
Itilon <ivanov.elect@gmail.com>
2017-07-28 01:02:33 +0300

    Signup form: slightly better animation

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,37 +1,10 @@
 /* globals $ */
 
 const button = $('#create-button');
-const logupDiv = $('#logup-div');
+const form = $('#logup-form');
 
 button.click( () => {
-    button.remove();
-    logupDiv
-        .append(`<form id="logup-form" 
-                class="form-group" action="/logup" 
-                method="post"></form>`);
-
-    const form = $('#logup-form');
-
-    form
-        .append('<label for="user">Please, select your username:</label>');
-    form
-        .append(`<input id="user" 
-                class="form-control" 
-                type="text" 
-                placeholder="Select your username" 
-                onfocus="this.placeholder = ''" 
-                onblur="this.placeholder = 'Enter your username'" 
-                name="username">`);
-    form
-        .append('<label for="pass">Please, choose a password:</label>');
-    form
-        .append(`<input id="pass" 
-                class="form-control" 
-                type="password" 
-                placeholder="Enter your password" 
-                onfocus="this.placeholder = ''" 
-                onblur="this.placeholder = 'Enter your password'" 
-                name="password">`);
-    form
-        .append('<button class="button" type="submit">Sign Up</button>');
+    button.fadeOut(2400, () => {
+        form.fadeIn(1200);
+    });
 });
