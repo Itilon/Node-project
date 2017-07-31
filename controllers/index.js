28c4c9b030581ab339490ed38913385b3b989118
stefanmanev <stef_manev@abv.bg>
2017-07-31 11:06:24 +0300

    small

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 module.exports = (data) => {
-    const postController = require('./post-controller')(data);
-    const userController = require('./user-controller')(data);
+    const postController = require('./post.controller')(data);
+    const userController = require('./user.controller')(data);
 
     const controller = {
         postController,
