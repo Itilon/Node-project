a30f60941c40707614dc426a2c6ead2b8b3c16b5
stefanmanev <stef_manev@abv.bg>
2017-07-31 18:01:08 +0300

    restore

--- app/config/port.config.js
+++ app/config/port.config.js
@@ -1,7 +1,7 @@
 /* globals process */
 // eslint-disable-next-line no-process-env
 const port = process.env.PORT || 3001;
-const test = 123131231;
+
 const connectionString = 'mongodb://localhost/blogging-db';
 
 module.exports = {
