df026443e66c46faf997487d10f0a33ce105047d
stefanmanev <stef_manev@abv.bg>
2017-08-01 02:43:03 +0300

    last

--- app/config/port.config.js
+++ app/config/port.config.js
@@ -1,5 +1,5 @@
 const port = 80;
-const connectionString = 'mongodb://172.31.44.130/blogging-db';
+const connectionString = 'mongodb://172.31.44.130:27017/blogging-db';
 
 module.exports = {
     port,
